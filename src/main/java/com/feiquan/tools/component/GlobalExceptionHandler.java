9301f7541c50d0c1ca6ab966e8860125d68e8d47
tyler <17765678689@163.com>
2025-11-28 17:34:46 +0800

    增加全局异常处理

---
+++ src/main/java/com/feiquan/tools/component/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.feiquan.tools.component;
+
+import com.feiquan.tools.constant.WtRespCode;
+import com.feiquan.tools.dto.api.WtResponse;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 处理参数校验异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public WtResponse<?> handleValidationException(MethodArgumentNotValidException ex) {
+        // 收集字段错误列表
+        List<WtResponse.ErrorDetail> errorDetails = Lists.newArrayList();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(err -> {
+                    WtResponse.ErrorDetail detail = WtResponse.ErrorDetail.builder()
+                            .field(err.getField())
+                            .msg(err.getDefaultMessage())
+                            .build();
+                    errorDetails.add(detail);
+                });
+        // 返回统一失败响应，包含错误明细
+        return WtResponse.failed(WtRespCode.Failed, errorDetails);
+    }
+
+    /**
+     * 捕获自定义业务异常（可选）
+     */
+//    @ExceptionHandler(BusinessException.class)
+//    public WtResponse<?> handleBusinessException(BusinessException ex) {
+//        return WtResponse.fail(ex.getCode(), ex.getMessage());
+//    }
+
+    /**
+     * 捕获所有未处理的异常
+     */
+    @ExceptionHandler(Exception.class)
+    public WtResponse<?> handleException(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return WtResponse.failed(WtRespCode.SystemExp);
+    }
+}
--- src/main/java/com/feiquan/tools/constant/WtRespCode.java
+++ src/main/java/com/feiquan/tools/constant/WtRespCode.java
@@ -3,6 +3,8 @@ package com.feiquan.tools.constant;
 public record WtRespCode(String code, String msg) {
 
     public static WtRespCode Succeed = new WtRespCode("0000", "succeed");
+    public static WtRespCode ParameterExp = new WtRespCode("0400", "参数校验未通过");
+    public static WtRespCode SystemExp = new WtRespCode("0500", "内部系统异常");
     public static WtRespCode Failed = new WtRespCode("9999", "failed");
 
     public static WtRespCode failedWithMsg(String msg) {
